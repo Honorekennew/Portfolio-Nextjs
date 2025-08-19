0797ce4927430e482bbf2ae7a0861db7a76e3bfe
Honoré kenne w <kennewhonore0@icloud.com>
2025-08-20 00:14:26 +0200

    edit MobileWarning

--- src/app/components/MobileWarning.tsx
+++ src/app/components/MobileWarning.tsx
@@ -20,19 +20,19 @@ export default function MobileWarning({ children }: { children: React.ReactNode 
   if (isMobile) {
     return (
       <div
-  className="flex flex-col items-center justify-center h-screen text-center px-6 bg-black text-white font-bold text-lg"
-  style={{ fontFamily: "var(--font-inter)" }}
->
-  <img
-    src="Firstpick4.jpg"
-    alt="logo"
-    className="p-1 rounded-full bg-[#252529] mb-4 object-cover"
-    style={{ width: 150, height: 150 }}
-  />
-  <span className="text-gray-300 text-2xl leading-8">
-    Hey ! Mon portfolio n’est pas encore responsive, mais le sera bientôt. Allez sur PC pour le moment. Merci !!!
-  </span>
-</div>
+        className="flex flex-col items-center justify-center h-screen text-center px-6 bg-black text-white font-bold text-lg"
+        style={{ fontFamily: "var(--font-inter)" }}
+      >
+        <img
+          src="Firstpick4.jpg"
+          alt="logo"
+          className="p-1 rounded-full bg-[#252529] mb-4 object-cover"
+          style={{ width: 150, height: 150 }}
+        />
+        <span className="text-gray-200 text-2xl leading-8">
+          Bonjour ! Mon portfolio n'est pas encore optimisé pour les appareils mobiles, mais il le sera bientôt. En attendant, merci de le consulter depuis un ordinateur Merci !!!
+        </span>
+      </div>
     );
   }
 
--- src/app/figma/page.tsx
+++ src/app/figma/page.tsx
@@ -11,51 +11,51 @@ export default function Figma() {
         {
             imageSrc: "/Figma1.png",
             title: "Portfolio UI",
-            link: "https://www.figma.com/design/a7BlMlrz5yweJSUhZcVr9u/Untitled?node-id=0-1&t=zzoYosCfVmC8hdeI-1",
+            link: "https://www.figma.com/design/a7BlMlrz5yweJSUhZcVr9u/Untitled?node-id=0-1&t=CvFYnPRXUxWltjDg-1",
             description: "Designed a clean and modern portfolio layout using Figma to showcase personal projects and skills."
         },
         {
             imageSrc: "/Figma5.png",
             title: "Schedule web UI",
-            link: "https://www.figma.com/design/ax3st8f8YmCo2LOSkoY9q9/Untitled?node-id=0-1&p=f&t=CeyJH8MbQi4u3i3D-0",
+            link: "https://www.figma.com/design/IaRKoHSFUmK5YE2kMiF1Lc/Untitled?node-id=0-1&t=aldGdV5YHNMI4Ni4-1",
             description: "Designed a user-friendly web application in Figma for seamless appointment booking, with a focus on clarity and intuitive workflows."
         },
         {
             imageSrc: "/Figma3.png",
             title: "Iba Channel mobile UI",
-            link: "https://www.figma.com/design/ax3st8f8YmCo2LOSkoY9q9/Untitled?node-id=0-1&p=f&t=CeyJH8MbQi4u3i3D-0",
+            link: "https://www.figma.com/design/ax3st8f8YmCo2LOSkoY9q9/Untitled?node-id=0-1&t=zmr2Hu1HCi24SGti-1",
             description: "Created a visually engaging and modern TV channel interface in Figma, focusing on user experience and intuitive navigation."
         },
 
         {
             imageSrc: "/Figma2.png",
             title: "AirPods UI",
-            link: "https://www.figma.com/design/3xjZL9ezdx38IbnWZdC0Wk/Untitled?node-id=0-1&p=f&t=jWjfScOxCxaHNZbt-0",
+            link: "https://www.figma.com/design/3xjZL9ezdx38IbnWZdC0Wk/Untitled?node-id=0-1&t=p4hzsKQafFquxS4J-1",
             description: "Designed a sleek and modern product showcase layout in Figma to highlight AirPods features and design details."
         },
         {
             imageSrc: "/Figma7.png",
             title: "AirPods Max UI",
-            link: "https://www.figma.com/design/pUcqG6Zl4wPsD89syNj6Gk/Untitled?node-id=0-1&p=f&t=PbYxxXQ97i4d9ywW-0",
+            link: "https://www.figma.com/design/pUcqG6Zl4wPsD89syNj6Gk/Untitled?node-id=0-1&t=UBo55IH3yxQgLoKg-1",
             description: "Designed a modern and elegant concept for AirPods Max in Figma."
         },
         {
             imageSrc: "/Figma6.png",
             title: "FruitFizz UI",
-            link: "https://www.figma.com/design/LQiGDb3VdfnUFlkVdXrQxC/Untitled?node-id=0-1&p=f&t=kQVLHjKdRUnee7y5-0",
+            link: "https://www.figma.com/design/LQiGDb3VdfnUFlkVdXrQxC/Untitled?node-id=0-1&t=OpDsW6FBEvN93jGG-1",
             description: "Created a playful and colorful fruit can design in Figma, experimenting with branding and visual identity just for fun."
         },
         {
             imageSrc: "/Figma8.png",
             title: "Nike UI",
-            link: "https://www.figma.com/design/pfyG1z2aZ0Zp5Yd56OPNun/Untitled?node-id=0-1&p=f&t=Q69kF6mFo38hHgUY-0",
+            link: "https://www.figma.com/design/pfyG1z2aZ0Zp5Yd56OPNun/Untitled?node-id=0-1&t=RYJDylvuW4qR6FVS-1",
             description: "Created a sleek and dynamic Nike design concept in Figma, exploring branding and visual identity with a modern touch."
         },
     ]
     return (
         <>
             <MobileWarning>
-                <section className="section-default relative" >
+                <section className="section-default relative">
                     <FadeInOnScroll>
                         <Menu />
                         <Illustration />
